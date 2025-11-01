f7b9ef3d6e27dc1ce3d0daf220e5fd1b842336c0
hmmini <pak4184@naver.com>
2025-11-01 15:27:21 +0900

    first commit

---
+++ src/main/java/io/springpragmaticpractices/SpringPragmaticPracticesApplication.java
@@ -0,0 +1,13 @@
+package io.springpragmaticpractices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPragmaticPracticesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPragmaticPracticesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/springpragmaticpractices/SpringPragmaticPracticesApplicationTests.java
@@ -0,0 +1,13 @@
+package io.springpragmaticpractices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPragmaticPracticesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
