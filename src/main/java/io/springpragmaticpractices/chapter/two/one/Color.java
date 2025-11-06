f35035fbe81c143789a1c407e5bfacbd7fc3c0c2
hmmini <pak4184@naver.com>
2025-11-06 19:00:00 +0900

    1부 객체지향 - 2. 객체의 종류 - VO

---
+++ src/main/java/io/springpragmaticpractices/chapter/two/one/Color.java
@@ -0,0 +1,34 @@
+package io.springpragmaticpractices.chapter.two.one;
+
+import java.util.Objects;
+
+public class Color {
+
+    public final int r;
+    public final int g;
+    public final int b;
+
+    public Color(int r, int g, int b) {
+        if (r < 0 || r > 255 ||
+            g < 0 || g > 255 ||
+            b < 0 || b > 255) {
+            throw new IllegalArgumentException("RGB shold be 0 to 255");
+        }
+        this.r = r;
+        this.g = g;
+        this.b = b;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Color color = (Color) o;
+        return r == color.r && g == color.g && b == color.b;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(r, g, b);
+    }
+}
