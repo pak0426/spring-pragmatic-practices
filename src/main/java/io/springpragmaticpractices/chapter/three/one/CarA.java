ae3aff873d66fae6ab1f54f5250d54c68325a01f
hmmini <pak4184@naver.com>
2025-11-08 19:00:00 +0900

    3부 행동 - 3.2 행동과 구현

---
+++ src/main/java/io/springpragmaticpractices/chapter/three/one/CarA.java
@@ -0,0 +1,13 @@
+package io.springpragmaticpractices.chapter.three.one;
+
+import javax.print.attribute.standard.MediaSize;
+import java.awt.*;
+import java.util.List;
+
+public class CarA {
+    public Frame frame;
+    public Engine engineering;
+    public List<Wheel> wheels;
+    public float speed;
+    public float direction;
+}
---
+++ src/main/java/io/springpragmaticpractices/chapter/three/one/CarB.java
@@ -0,0 +1,8 @@
+package io.springpragmaticpractices.chapter.three.one;
+
+public class CarB {
+    public void drive() {}
+    public void changeDirection() {}
+    public void accelerate(float speed) {}
+    public void decelerate(float speed) {}
+}
