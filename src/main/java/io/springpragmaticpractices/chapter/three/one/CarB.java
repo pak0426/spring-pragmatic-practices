aec721cd02d01b5fcefd667d6a9623375cccfffb
hmmini <pak4184@naver.com>
2025-11-09 19:00:00 +0900

    3부 행동 - 3.3 인터페이스

--- src/main/java/io/springpragmaticpractices/chapter/three/one/CarB.java
+++ src/main/java/io/springpragmaticpractices/chapter/three/one/CarB.java
@@ -1,8 +1,17 @@
 package io.springpragmaticpractices.chapter.three.one;
 
 public class CarB {
+
+    private int degree;
+
     public void drive() {}
-    public void changeDirection() {}
+    public float changeDirection(int amount) {
+        float result = (degree + amount) % 360;
+        if (result < 0) {
+            result += 360;
+        }
+        return result;
+    }
     public void accelerate(float speed) {}
     public void decelerate(float speed) {}
 }
---
+++ src/main/java/io/springpragmaticpractices/chapter/three/one/CarB2.java
@@ -0,0 +1,9 @@
+package io.springpragmaticpractices.chapter.three.one;
+
+interface CarB2 {
+
+    void drive();
+    float changeDirection(int amount);
+    void accelerate(float speed);
+    void decelerate(float speed);
+}
