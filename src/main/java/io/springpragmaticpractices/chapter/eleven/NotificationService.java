74fd8b2770556297e9c718f3a9b9a8dbd3273d44
hmmini <pak4184@naver.com>
2026-03-07 14:27:18 +0900

    notify 메서드는 계정에 들록된 알림 채널이 무엇인지 확인하고, 이와 일치하는 알림 채널에 알림을 보낸다.

--- src/main/java/io/springpragmaticpractices/chapter/eleven/NotificationService.java
+++ src/main/java/io/springpragmaticpractices/chapter/eleven/NotificationService.java
@@ -3,11 +3,22 @@ package io.springpragmaticpractices.chapter.eleven;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class NotificationService {
 
-    private final NotificationChannel notificationChannel;
+    private final EmailNotificationChannel emailChannel;
+    private final SlackNotificationChannel slackChannel;
+    private final ChatNotificationChannel chatChannel;
 
-    //
+    public void notify(Account account, String message) {
+        switch (account.getNotificationType()) {
+            case EMAIL -> emailChannel.send(account, message);
+            case SLACK -> slackChannel.send(account, message);
+            case CHAT  -> chatChannel.send(account, message);
+            default    -> throw new IllegalArgumentException("지원하지 않는 알림 타입");
+        }
+    }
 }
