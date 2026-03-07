54e7a9a542171e0084997583dcb00edaa3841ea6
hmmini <pak4184@naver.com>
2026-03-07 18:14:03 +0900

    사용자가 시스템에 가입하는 상황을 표현하는 UserService

---
+++ src/main/java/io/springpragmaticpractices/chapter/forteen/UserService.java
@@ -0,0 +1,32 @@
+package io.springpragmaticpractices.chapter.forteen;
+
+import lombok.Builder;
+import lombok.RequiredArgsConstructor;
+import org.apache.catalina.User;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
+
+@Service
+@Builder
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final VerificationEmailSender verificationEmailSender;
+
+    @Transactional
+    public User register(UserCreateDto userCreateDto) {
+        User user = User.builder()
+                .email(userCreateDto.getEmail())
+                .nickname(userCreateDto.getNickname())
+                .status(UserStatus.PENDING)
+                .verificationCode(UUID.randomUUID().toString())
+                .build();
+
+        user = userRepository.save(user);
+        verificationEmailSender.send(user);
+        return user;
+    }
+}
