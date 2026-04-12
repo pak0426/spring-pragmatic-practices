0347585ed84cee9d3a3f24e35deebd36d62ba4e3
hmmini <pak4184@naver.com>
2026-04-12 17:00:53 +0900

    리스트 이벤트를 반환하는 login 메서드

--- src/main/java/io/springpragmaticpractices/chapter/fifteen/one/User.java
+++ src/main/java/io/springpragmaticpractices/chapter/fifteen/one/User.java
@@ -1,9 +1,13 @@
 package io.springpragmaticpractices.chapter.fifteen.one;
 
+import jdk.jfr.Event;
+import jdk.jfr.EventType;
 import lombok.Builder;
 import lombok.Getter;
 
 import java.time.Clock;
+import java.util.Collections;
+import java.util.List;
 
 @Getter
 @Builder
@@ -12,11 +16,12 @@ public class User {
     private String email;
     private long lastLoginTimestamp;
 
-    public LoginSuccess login(ClockHolder clockHolder) {
+    public List<Event> login(ClockHolder clockHolder) {
         // ...
         this.lastLoginTimestamp = clockHolder.now();
-        return LoginSuccess.builder()
-                .auditMessage("User(" + email + ") login!")
-                .build();
+        return Collections.singletonList(Event.builder()
+                .type(EventType.AUDIT_LOG)
+                .message("User(" + email + ") login!")
+                .build());
     }
 }
